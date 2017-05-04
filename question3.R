5b2578c7fc741a0a2e25b045b3c2a66ec006b8c7
Stéphane <stephane.dong@outlook.fr>
2017-05-04 19:01:37 +0200

    q3 final

--- question3.R
+++ question3.R
@@ -1,11 +1,7 @@
-ordo <- c()
-absc <- c()
-n <- 20000
-i <- 1
-while (i < n) {
-  ordo[i] = i/n
-  i = i + 1
-}
+p <- 0.25
+n <- 5000
+ordo <- seq(1/n,1,by=1/n)
+absc <- sort(rgeom(n,p))
 
 h <- function(x) {
   return(log(1-x))
@@ -13,8 +9,11 @@ h <- function(x) {
 
 i <- 1
 while (i < n) {
-  absc[i] <- h(ordo[i])
+  ordo[i] <- h(ordo[i])
   i = i + 1
 }
-plot(ordo, main="Graphe de probabilité de la loi géométrique", absc, xlab="ln(1 - i/n)", ylab="i/n")
+
+plot(absc, ordo, main="Graphe de probabilité de la loi géométrique", xlab="xi*", ylab="ln(1-i/n)")
 print(lm(ordo ~ absc))
+print("pente : ")
+print((1-exp(lm(ordo~absc)$coefficients[2])))
